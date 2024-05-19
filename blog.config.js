ff2ccee88b1cc3e85a00b596bc3ed6449803300e
joffeego <80955472+joffeego@users.noreply.github.com>
2024-05-19 19:49:45 +0800

    Update blog.config.js
    update blog config

--- blog.config.js
+++ blog.config.js
@@ -1,12 +1,12 @@
 const BLOG = {
   title: 'JOFFEE',
   author: 'joffeego',
-  email: 'joffee@email.cn',
+  email: '3108574816@qq.com',
   link: 'https://www.joffee.eu.org/',
   description: 'It gonna be awesome.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'auto', // ['light', 'dark', 'auto'],
+  appearance: 'light', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#EEEDE7', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#868B8E', // use hex value, don't forget '#'
@@ -18,7 +18,7 @@ const BLOG = {
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
-  socialLink: 'https://twitter.com/craigaryhart',
+  socialLink: '',
   seo: {
     keywords: ['Blog', 'Website', 'Notion'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
