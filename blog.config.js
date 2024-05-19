a27b5931a74a4c8d19e870f1e5842d496d5ff4d3
joffeego <80955472+joffeego@users.noreply.github.com>
2024-05-19 19:35:30 +0800

    Update blog.config.js
    更新blog配置

--- blog.config.js
+++ blog.config.js
@@ -3,7 +3,7 @@ const BLOG = {
   author: 'joffeego',
   email: 'joffee@email.cn',
   link: 'https://www.joffee.eu.org/',
-  description: 'This gonna be an awesome website.',
+  description: 'It gonna be awesome.',
   lang: 'zh-CN', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
   timezone: 'Asia/Shanghai', // Your Notion posts' date will be interpreted as this timezone. See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
   appearance: 'auto', // ['light', 'dark', 'auto'],
